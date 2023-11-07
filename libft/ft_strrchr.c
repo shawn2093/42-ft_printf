58fe492a221b5e6cc42ec02d514c108514cde3fa
shawn2093 <shawnong1993@gmail.com>
2023-11-08 05:57:14 +0800

    update libft

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: long <long@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 18:43:33 by long              #+#    #+#             */
-/*   Updated: 2023/10/20 16:56:02 by long             ###   ########.fr       */
+/*   Updated: 2023/11/08 05:56:29 by long             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 char	*ft_strrchr(char *s, int c)
 {
 	char	*ptr;
-	size_t	i;
+	int		i;
 
-	i = ft_strlen(s);
-	while (i >= 0)
+	i = (int) ft_strlen(s);
+	while (i > 0)
 	{
 		ptr = &s[i];
 		if (s[i] == (unsigned char) c)
 			return (ptr);
-		if (i == 0)
-			return (0);
 		i--;
 	}
 	return (0);
